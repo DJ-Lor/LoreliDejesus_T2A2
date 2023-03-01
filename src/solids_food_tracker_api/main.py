8de82c9087d3c03d48550ed60695221322f5a8ee
Loreli <dejesus.loreli@gmail.com>
2023-03-01 15:21:33 +1100

    inital setup

--- src/solids_food_tracker_api/main.py
+++ src/solids_food_tracker_api/main.py
@@ -1,13 +1,23 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
 
+db = SQLAlchemy()
+ma = Marshmallow()
 
 def create_app():
     #initialise flask app
     app = Flask(__name__)
 
-
     #add configuration from object
     app.config.from_object("config.app_config")
 
+    db.init_app(app)
+    ma.init_app(app)
+
+    from controller import registerable_controllers
+    
+    for controller in registerable_controllers:
+        app.register_blueprint(controller)
 
     return app
